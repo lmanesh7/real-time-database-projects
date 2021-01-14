21834ec2e26563035bb59a59b0313466b3570676
lmanesh7 <47899760+lmanesh7@users.noreply.github.com>
2021-01-14 23:02:39 +0530

    Update testdbstream.py

--- testdbstream.py
+++ testdbstream.py
@@ -33,7 +33,7 @@ for i in us.values():
         vals_list.append(i[j])
 df = pd.DataFrame({"User":keys_list,"Login Time":vals_list})
 df['Login'] = pd.to_datetime(df['Login Time'])
-df['Login']=df['Login'].dt.tz_localize('Asia/Kolkata')
+df['login']=df['Login'].dt.tz_localize('Asia/Kolkata')
 
 
 
