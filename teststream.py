8ea3c6a4191d85771b3063ffa86ac0fac12fdd94
lmanesh7 <47899760+lmanesh7@users.noreply.github.com>
2021-01-14 22:16:52 +0530

    Update teststream.py

--- teststream.py
+++ teststream.py
@@ -6,13 +6,13 @@ df = pd.read_csv("sensor.csv")
 
 #df1 = df.drop(['unnamed'],axis=1)
  
-st.write(" " "Manesh's Water Level Monitoring!" " ")
+st.write(" " "Manesh's Room Humidity Monitoring!" " ")
 
-st.write("Water level chart")
+st.write("Humidity level chart")
 st.line_chart(df['y'])
 st.write("Entire HighLighted DB")
 st.dataframe(df.style.highlight_max(axis=0))
-l=[0,10,20,40,70,95]
+l=[0,10,20,25,40,50,70,75,95]
 st.write("You can check some border equal values using the frequently checked values below")
 n=st.radio("frequently checked values",l)
 if n in l:
@@ -32,12 +32,15 @@ df['z'] = pd.to_datetime(df['z'],format="%d-%m-%Y")
 
 d3 = st.date_input("range",[dt.date(2021,1,9) , dt.date(2021,1,9)])
 #st.write(d3)
-min1 = d3[0]
-max1 = d3[1]
-if max1>=min1:
-    st.write(df.query("@min1<=z<=@max1"))
-else:
-    st.write("To date is shorter than from date ")
+try:
+   min1 = d3[0]
+   max1 = d3[1]
+   if max1>=min1:
+       st.write(df.query("@min1<=z<=@max1"))
+   else:
+       st.write("To date is shorter than from date ")
+except IndexError:
+   st.write("Enter two range dates")
 
 pic ="PSX_20201122_184952.jpg"
 
