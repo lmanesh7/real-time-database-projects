fba61aabd02713e3513c8743c2e1f5c499cfd4d2
lmanesh7 <47899760+lmanesh7@users.noreply.github.com>
2021-01-18 22:56:35 +0530

    Update teststream.py

--- teststream.py
+++ teststream.py
@@ -17,9 +17,7 @@ st.write("Humidity level chart")
 st.line_chart(df['y'])
 t = df['y'].tail(1)
 data = px.data.gapminder()
-fig = px.bar(data[df['y]], x='year', y='pop',
-             hover_data=['x', 'y'], color='lifeExp',
-             labels={'pop':'population of Canada'}, height=400)
+fig = px.bar(data[df['y]],hover_data=['x', 'y'], color='lifeExp',labels={'pop':'population of Canada'}, height=400)
 fig.show()
 '''fig = go.Figure(y=df['y'])
 fig.update_layout(autosize=True,width=800, height=800,margin=dict(l=40, r=40, b=40, t=40))'''
